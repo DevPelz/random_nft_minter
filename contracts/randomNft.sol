122a4178de74717cdbe18e3f931b53f039e73766
Dev_Pelz <pelzfx1@gmail.com>
2023-09-14 14:56:54 +0100

    modified: contracts/randomNft.sol

--- contracts/randomNft.sol
+++ contracts/randomNft.sol
@@ -19,18 +19,25 @@ contract CHIX is ERC721Enumerable, Ownable, VRFConsumerBaseV2 {
     // Goerli/chainlink vars for randommness based on network specifications
     // Chainlink VRF Variables
     VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
-    uint64 private immutable i_suscriptionId;
-    bytes32 private immutable i_gasLane;
-    uint32 private immutable i_callbackGasLimit;
+    address private constant vrfCoordinatorV2 =
+        0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D;
+    uint64 private constant suscriptionId = 14214;
+    bytes32 private constant gasLane =
+        0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
+    uint32 private constant callbackGasLimit = 2_500_000;
     uint16 private constant REQUEST_CONFIRMATIONS = 3;
     uint32 private constant NUM_WORDS = 1;
 
+    // NFT variables
+    uint256 public constant mintFee = 0.000001 ether;
+    uint256 private _tokenCounter;
+    uint256 internal constant MAX_TRAIT_VAL = 100;
+    string[] internal _nftTokenUris;
+    bool private _initialized;
+
     constructor(
-        address vrfCoordinatorV2,
-        uint64 suscriptionId,
-        bytes32 gasLane,
-        uint256 mintFee,
-        uint32 callbackGasLimit,
         string[3333] memory nftTokenUris
-    ) VRFConsumerBaseV2(vrfCoordinatorV2) ERC721("DigitalVistas", "DV") {}
+    ) VRFConsumerBaseV2(vrfCoordinatorV2) ERC721("DigitalVistas", "DV") {
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+    }
 }
